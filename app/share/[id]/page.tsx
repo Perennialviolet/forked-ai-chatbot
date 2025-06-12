98eb4197f4595bb1aa2a31e1981a2610cab2d2ca
Perennialviolet <trainabull@gmail.com>
2025-06-12 11:07:56 -0500

    Update page.tsx

--- app/share/[id]/page.tsx
+++ app/share/[id]/page.tsx
@@ -7,7 +7,6 @@ import { getSharedChat } from '@/app/actions'
 import { ChatList } from '@/components/chat-list'
 import { FooterText } from '@/components/footer'
 
-export const runtime = 'nodejs'
 export const preferredRegion = 'home'
 
 interface SharePageProps {
